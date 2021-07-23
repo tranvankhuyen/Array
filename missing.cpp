65eb02d96052f82be503b4aa9469cee60d09e75e
unknown <khuyen10a5k61@gmail.com>
2021-07-23 23:36:38 +0700

    demo

---
+++ missing.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+// main function
+int main(){
+    int a[5] = {1,2,3,4,5};
+    int b[5] = {1,3,4,5};
+    int n(5);
+    for(int i = 0; i <= n - 1; i++)
+    {
+        if(a[i] != a[j])
+        {
+            std::cout<<a[i];
+            break;
+        }
+    }
+    return 0;
+}
