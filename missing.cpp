b7b1e1433d18534c8a7e48fa7f7b9608c60d3d2c
unknown <khuyen10a5k61@gmail.com>
2021-07-24 16:01:52 +0700

    fix2

--- missing.cpp
+++ missing.cpp
@@ -5,15 +5,23 @@
 // main function
 int main(){
     int a[5] = {1,2,3,4,5};
-    int b[5] = {1,3,4,5};
+    int b[5] = {5,3,1};
     int n(5);
     for(int i = 0; i <= n - 1; i++)
     {
-        if(a[i] != b[i])
+        int t = 0;
+        for(int j = 0; j <=n; j++)
         {
-            std::cout<<a[i];
-            break;
+            if (a[i] == b[j])
+            {
+                t = 1;
+            }
         }
+        if(t == 0)
+        {
+            std::cout<<a[i]<<" ";
+        }
+
     }
     return 0;
 }
