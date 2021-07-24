45f05b450c6dceca7108311d9b7850fe74053944
unknown <khuyen10a5k61@gmail.com>
2021-07-24 22:27:02 +0700

    new update

--- element_appear_once.cpp
+++ element_appear_once.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include<time.h>
+#include <iomanip>
 void appear(int a[], int n)
 {
     int b[20];
@@ -27,6 +28,12 @@ void appear(int a[], int n)
 int main(){
     int a[10] = {1,2,3,4,2,3,4,5,6,6};
     int n(10);
+    double time_use ;
+    clock_t start, end;
+    start = clock();
     appear(a, n);
+    end = clock();
+    time_use = (double)(end-start) / CLOCKS_PER_SEC;
+    std::cout<<std::fixed<<std::setprecision(20)<<std::endl<<time_use<<std::endl;
     return 0;
 }
